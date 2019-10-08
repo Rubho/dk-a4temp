9f93bf0a6370a8f1806e602d30b57f8ad3bdcca1
GustavSB <Gustav.strengen@gmail.com>
2019-10-08 12:32:30 +0200

    to do 6 v1 think i am done
    v1

--- ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
+++ ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
@@ -166,7 +166,16 @@ public class TCPClient {
         // TODO Step 6: Implement this method
         // Hint: Reuse sendCommand() method
         // Hint: update lastError if you want to store the reason for the error.
-        return false;
+        boolean result= false;
+        if (recipient!= null){
+            sendCommand("privmsg " + recipient + " " + message);
+
+        }else{
+            lastError= "Could not send Private Message, because recipient does not exist.";
+            System.out.println(lastError);
+            result=false;
+        }
+        return result;
     }
 
 
